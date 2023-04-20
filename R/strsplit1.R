c0da84f35e65848ae95e5d086ad1d099aaf12471
CanElverici <elverici.can@gmail.com>
2023-04-19 23:54:21 -0500

    "v0.0.1 documentation update"

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,3 +1,16 @@
+#' Split a string
+#'
+#' @param x A character vector with one element
+#' @param split What to split on
+#' 
+#' @return A character vector
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' x <- "a,b,c"
+#' strsplit1(x, split = ",")
+#' }
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
